1a4faad42df9b10b748efdb0d2950f1b0080b8f4
jose mauricio delgado <jdelgado2002@gmail.com>
2016-12-07 15:03:47 -0600

    moving to firebase

--- js/inversiones.js
+++ js/inversiones.js
@@ -1,19 +1,35 @@
-// jQuery snippet for changing HTML form into JSON
-$('#investorForm').submit(function(e) {
+$(document).ready(setFirebase);
+
+function setFirebase() {
+    var config = {
+        apiKey: "AIzaSyC_DFth0P7ZONo5H8LMsV04f9iFmlEwoxQ",
+        authDomain: "elmis-81b8f.firebaseapp.com",
+        databaseURL: "https://elmis-81b8f.firebaseio.com",
+        storageBucket: "elmis-81b8f.appspot.com",
+        messagingSenderId: "884585399306"
+    };
+    firebase.initializeApp(config);
+};
+
+function writeUserData(name, email) {
+  firebase.database().ref('leads/').push({
+    username: name,
+    email: email
+  });
+}
+
+$('#investorForm').submit(function (e) {
     // prevent default submiting form
     e.preventDefault();
-    //analytics
-    ga('send', 'event', 'Buttons', 'click', 'inversion'); 
-    // serialize data to JSON
-    $.post("https://sheetsu.com/apis/v1.0/cdaa04467cd6",{
-        InputName: $("#InputName").val(),
-        InputEmail: $("#InputEmail").val()
-    },
-    function (data, status){
-        toastr.success('Gracias nos comunicaremos muy pronto!', $("#InputName").val());     
-        $("#InputName").val("");
-        $("#InputEmail").val("");
-    }
-          );
-      return false;
-    });
+    
+    ga('send', 'event', 'Buttons', 'click', 'inversion');
+    
+    var InputName = $("#InputName").val();
+    var InputEmail = $("#InputEmail").val();
+    writeUserData(InputName, InputEmail);
+    
+    toastr.success('Gracias nos comunicaremos muy pronto!', $("#InputName").val());
+    $("#InputName").val("");
+    $("#InputEmail").val("");
+    return false;
+});
